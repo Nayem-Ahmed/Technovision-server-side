8c48df21ea239761131d022ef19941b746cf1e69
unknown <freelancernayem.me@gmail.comm>
2024-01-17 09:30:54 +0600

    delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
- 
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.8wqrrau.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,77 +27,82 @@ async function run() {
     const productsCollection = client.db('Technovission').collection('products')
     const addCartCollection = client.db('Technovission').collection('addCart')
 
-     // Save or modify user email, status in DB
-     app.put('/users/:email', async (req, res) => {
-        const email = req.params.email
-        const user = req.body
-        const query = { email: email }
-        const options = { upsert: true }
-        const isExist = await usersCollection.findOne(query)
-        console.log('User found?----->', isExist)
-        if (isExist) {
-          if (user?.status === 'Requested') {
-            const result = await usersCollection.updateOne(
-              query,
-              {
-                $set: user,
-              },
-              options
-            )
-            return res.send(result)
-          } else {
-            return res.send(isExist)
-          }
+    // Save or modify user email, status in DB
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const isExist = await usersCollection.findOne(query)
+      console.log('User found?----->', isExist)
+      if (isExist) {
+        if (user?.status === 'Requested') {
+          const result = await usersCollection.updateOne(
+            query,
+            {
+              $set: user,
+            },
+            options
+          )
+          return res.send(result)
+        } else {
+          return res.send(isExist)
         }
-        const result = await usersCollection.updateOne(
-          query,
-          {
-            $set: { ...user, timestamp: Date.now() },
-          },
-          options
-        )
-        res.send(result)
-      })
+      }
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      )
+      res.send(result)
+    })
 
     // post(add products) products
-      app.post('/products', async (req, res) => {
-        const newproduct = req.body;
-        const result = await  productsCollection.insertMany(newproduct)
-        res.send(result)
-      })
+    app.post('/products', async (req, res) => {
+      const newproduct = req.body;
+      const result = await productsCollection.insertMany(newproduct)
+      res.send(result)
+    })
 
-      // Get all products
-      app.get('/products', async (req, res) => {
-        const result = await productsCollection.find().toArray()
-        res.send(result)
-      })
+    // Get all products
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray()
+      res.send(result)
+    })
 
-      // Get single products
-      app.get('/products/:id', async (req, res) => {
-        const id = req.params.id
-        const result = await productsCollection.findOne({ _id: new ObjectId(id) })
-        res.send(result)
-      })
+    // Get single products
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await productsCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
 
     // addCart in database(post)
     app.post('/addcart', async (req, res) => {
-        const newpusers = req.body;
-        const result = await  addCartCollection.insertMany(newpusers)
-        res.send(result)
-      })
+      const newpusers = req.body;
+      const result = await addCartCollection.insertMany(newpusers)
+      res.send(result)
+    })
 
     // Get add cart data
     app.get('/addcart/:email', async (req, res) => {
       const email = req.params.email;
-      const query = {email:email}
-      const result = await  addCartCollection.find(query).toArray()
+      const query = { email: email }
+      const result = await addCartCollection.find(query).toArray()
       res.send(result)
     })
     // get users rool
     app.get('/userrole/:email', async (req, res) => {
       const email = req.params.email;
-      const query = {email:email}
-      const result = await  usersCollection.find(query).toArray()
+      const query = { email: email }
+      const result = await usersCollection.find(query).toArray()
+      res.send(result)
+    })
+    // Get all Users
+    app.get('/userrole', async (req, res) => {
+      const result = await usersCollection.find().toArray()
       res.send(result)
     })
 
